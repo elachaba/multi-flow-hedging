44b227127bc8c6e4afc330bb9236e02b51ee266d
elachaba <elachabaymane995@gmail.com>
2024-12-18 18:33:10 +0100

    Feat: implements the price function

--- Pricer/MonteCarlo/MonteCarlo.cpp
+++ Pricer/MonteCarlo/MonteCarlo.cpp
@@ -4,16 +4,18 @@
 void pricer::MonteCarlo::price(double& price, double& confidence_interval) {
 	double sum = 0.0;
 	double squared_sum = 0.0;
-	double payoff;
+	double r = model.getRiskFreeRate();
+	PnlVect* payoffs;
 	for (int i = 0; i < samples_number; i++) {
 		const PnlMat* const path = get_path();
-		
-		payoff = option.payoff(path); // actualisation dans les payoffs
+		double payoff = option.discounted_payoff(path, r, t);
 		sum += payoff;
-		squared_sum += payoff * payoff;
+		squared_sum = payoff * payoff;
 	}
 	price = sum / samples_number;
 	double variance = squared_sum / samples_number - price * price;
 	confidence_interval = 1.96 * sqrt(variance / samples_number);
+
+	pnl_vect_free(&payoffs);
 
 }
